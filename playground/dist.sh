2a768e27255cfc6a32119a644da5889cacb49175
bozo.kopic <bozo@kopic.xyz>
2022-05-27 23:51:09 +0200

    linux aarch64 support

--- playground/dist.sh
+++ playground/dist.sh
@@ -4,7 +4,7 @@ set -e
 
 . $(dirname -- "$0")/env.sh
 
-TARGET_PLATFORMS="linux windows"
+TARGET_PLATFORMS="linux_x86_64 linux_aarch64 windows_amd64"
 TARGET_PY_VERSIONS="cp38 cp39 cp310"
 
 cd $ROOT_PATH
