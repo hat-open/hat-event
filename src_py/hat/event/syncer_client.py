d7bcc143a55bada1859a62cd040bf12d859c2666
Jakov Krstulovic Opara <jakov.krstulovic@koncar.hr>
2022-06-30 14:43:30 +0200

    syncer client api update - name, syncer_token added

--- src_py/hat/event/syncer_client.py
+++ src_py/hat/event/syncer_client.py
@@ -1,3 +1,5 @@
+import typing
+
 from hat import aio
 from hat.event.server import common
 import hat.monitor.client
@@ -6,6 +8,8 @@ import hat.monitor.client
 async def create_syncer_client(backend: common.Backend,
                                monitor_client: hat.monitor.client.Client,
                                monitor_group: str,
+                               name: str,
+                               syncer_token: typing.Optional[str] = None,
                                **kwargs
                                ) -> 'SyncerClient':
     """Create syncer client
@@ -14,6 +18,8 @@ async def create_syncer_client(backend: common.Backend,
         backend: backend
         monitor_client: monitor client
         monitor_group: monitor group name
+        name: client name
+        syncer_token: syncer token
         kwargs: additional arguments passed to `hat.chatter.connect` coroutine
 
     """
