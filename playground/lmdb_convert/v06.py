cab81eee2ae6dfea63f3ab577c4025c3415ce217
bozo.kopic <bozo@kopic.xyz>
2022-07-27 22:57:29 +0200

    playground lmdb backend convert script

---
+++ playground/lmdb_convert/v06.py
@@ -0,0 +1,247 @@
+from pathlib import Path
+import enum
+import struct
+import platform
+import typing
+
+import lmdb
+
+from hat import chatter
+from hat import json
+from hat import sbs
+
+
+EventType: typing.Type = typing.Tuple[str, ...]
+"""Event type"""
+
+
+EventPayloadType = enum.Enum('EventPayloadType', [
+    'BINARY',
+    'JSON',
+    'SBS'])
+
+
+class EventId(typing.NamedTuple):
+    server: int
+    """server identifier"""
+    instance: int
+    """event instance identifier"""
+
+
+class SbsData(typing.NamedTuple):
+    module: typing.Optional[str]
+    """SBS module name"""
+    type: str
+    """SBS type name"""
+    data: bytes
+
+
+class EventPayload(typing.NamedTuple):
+    type: EventPayloadType
+    data: typing.Union[bytes, json.Data, SbsData]
+
+
+class Timestamp(typing.NamedTuple):
+    s: int
+    """seconds since 1970-01-01 (can be negative)"""
+    us: int
+    """microseconds added to timestamp seconds in range [0, 1e6)"""
+
+    def __lt__(self, other):
+        if not isinstance(other, Timestamp):
+            return NotImplemented
+        return self.s * 1000000 + self.us < other.s * 1000000 + other.us
+
+    def __gt__(self, other):
+        if not isinstance(other, Timestamp):
+            return NotImplemented
+        return self.s * 1000000 + self.us > other.s * 1000000 + other.us
+
+    def __eq__(self, other):
+        if not isinstance(other, Timestamp):
+            return NotImplemented
+        return self.s * 1000000 + self.us == other.s * 1000000 + other.us
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __le__(self, other):
+        return self < other or self == other
+
+    def __ge__(self, other):
+        return self > other or self == other
+
+    def __hash__(self):
+        return self.s * 1000000 + self.us
+
+
+class Event(typing.NamedTuple):
+    event_id: EventId
+    event_type: EventType
+    timestamp: Timestamp
+    source_timestamp: typing.Optional[Timestamp]
+    payload: typing.Optional[EventPayload]
+
+
+def decode_uint(x: bytes) -> int:
+    return struct.unpack(">Q", x)[0]
+
+
+def decode_timestamp(x: bytes) -> Timestamp:
+    res = struct.unpack(">QI", x)
+    return Timestamp(res[0] - (1 << 63), res[1])
+
+
+def decode_tuple_str(x: bytes) -> typing.Tuple[str, ...]:
+    return tuple(json.decode(str(x, encoding='utf-8')))
+
+
+def decode_json(x: bytes) -> json.Data:
+    return json.decode(str(x, encoding='utf-8'))
+
+
+def decode_uint_timestamp_uint(x: bytes
+                               ) -> typing.Tuple[int, Timestamp, int]:
+    res = struct.unpack(">QQIQ", x)
+    return res[0], Timestamp(res[1] - (1 << 63), res[2]), res[3]
+
+
+def decode_event(event_bytes: bytes) -> Event:
+    event_sbs = _sbs_repo.decode('HatEvent', 'Event', event_bytes)
+    return _event_from_sbs(event_sbs)
+
+
+def create_env(path: Path):
+    max_dbs = 5
+    max_db_size = (512 * 1024 * 1024 * 1024
+                   if platform.architecture()[0] == '64bit'
+                   else 1024 * 1024 * 1024)
+    return lmdb.Environment(str(path),
+                            map_size=max_db_size,
+                            subdir=False,
+                            max_dbs=max_dbs)
+
+
+_sbs_repo = sbs.Repository(chatter.sbs_repo, r"""
+module HatEvent
+
+MsgSubscribe = Array(EventType)
+
+MsgNotify = Array(Event)
+
+MsgRegisterReq = Array(RegisterEvent)
+
+MsgRegisterRes = Array(Union {
+    event:    Event
+    failure:  None
+})
+
+MsgQueryReq = QueryData
+
+MsgQueryRes = Array(Event)
+
+Timestamp = Tuple {
+    s:   Integer
+    us:  Integer
+}
+
+EventId = Tuple {
+    server:    Integer
+    instance:  Integer
+}
+
+Order = Union {
+    descending:  None
+    ascending:   None
+}
+
+OrderBy = Union {
+    timestamp:        None
+    sourceTimestamp:  None
+}
+
+EventType = Array(String)
+
+EventPayload = Union {
+    binary:  Bytes
+    json:    String
+    sbs:     Hat.Data
+}
+
+Event = Tuple {
+    id:               EventId
+    type:             EventType
+    timestamp:        Timestamp
+    sourceTimestamp:  Maybe(Timestamp)
+    payload:          Maybe(EventPayload)
+}
+
+RegisterEvent = Tuple {
+    type:             EventType
+    sourceTimestamp:  Maybe(Timestamp)
+    payload:          Maybe(EventPayload)
+}
+
+QueryData = Tuple {
+    ids:                Maybe(Array(EventId))
+    types:              Maybe(Array(EventType))
+    tFrom:              Maybe(Timestamp)
+    tTo:                Maybe(Timestamp)
+    sourceTFrom:        Maybe(Timestamp)
+    sourceTTo:          Maybe(Timestamp)
+    payload:            Maybe(EventPayload)
+    order:              Order
+    orderBy:            OrderBy
+    uniqueType:         Boolean
+    maxResults:         Maybe(Integer)
+}
+""")
+
+
+def _event_from_sbs(data: sbs.Data) -> Event:
+    return Event(
+        event_id=_event_id_from_sbs(data['id']),
+        event_type=tuple(data['type']),
+        timestamp=_timestamp_from_sbs(data['timestamp']),
+        source_timestamp=_optional_from_sbs(data['sourceTimestamp'],
+                                            _timestamp_from_sbs),
+        payload=_optional_from_sbs(data['payload'], _event_payload_from_sbs))
+
+
+def _event_payload_from_sbs(data: sbs.Data) -> EventPayload:
+    data_type, data_data = data
+
+    if data_type == 'binary':
+        return EventPayload(type=EventPayloadType.BINARY,
+                            data=data_data)
+
+    if data_type == 'json':
+        return EventPayload(type=EventPayloadType.JSON,
+                            data=json.decode(data_data))
+
+    if data_type == 'sbs':
+        return EventPayload(type=EventPayloadType.SBS,
+                            data=_sbs_data_from_sbs(data_data))
+
+    raise ValueError('unsupported payload type')
+
+
+def _timestamp_from_sbs(data: sbs.Data) -> Timestamp:
+    return Timestamp(s=data['s'], us=data['us'])
+
+
+def _event_id_from_sbs(data: sbs.Data) -> EventId:
+    return EventId(server=data['server'],
+                   instance=data['instance'])
+
+
+def _sbs_data_from_sbs(data: sbs.Data) -> SbsData:
+    return SbsData(module=_optional_from_sbs(data['module']),
+                   type=data['type'],
+                   data=data['data'])
+
+
+def _optional_from_sbs(data: sbs.Data,
+                       fn=lambda i: i
+                       ) -> typing.Optional[typing.Any]:
+    return fn(data[1]) if data[0] == 'Just' else None
---
+++ playground/lmdb_convert/v07.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import platform
+import typing
+
+import lmdb
+
+from hat import json
+from hat.event.server.backends.lmdb import common
+from hat.event.server.backends.lmdb import encoder
+
+EventType = common.EventType
+EventId = common.EventId
+EventPayloadType = common.EventPayloadType
+EventPayload = common.EventPayload
+Timestamp = common.Timestamp
+Event = common.Event
+DbType = common.DbType
+ServerId = common.ServerId
+EventTypeRef = common.EventTypeRef
+PartitionId = common.PartitionId
+LatestEventRef = common.LatestEventRef
+OrderedEventRef = common.OrderedEventRef
+EventRef = common.EventRef
+
+SystemDbKey = ServerId
+SystemDbValue = typing.Tuple[EventId, Timestamp]
+
+LatestDataDbKey = EventTypeRef
+LatestDataDbValue = Event
+
+LatestTypeDbKey = EventTypeRef
+LatestTypeDbValue = EventType
+
+OrderedDataDbKey = typing.Tuple[PartitionId, Timestamp, EventId]
+OrderedDataDbValue = Event
+
+OrderedPartitionDbKey = PartitionId
+OrderedPartitionDbValue = json.Data
+
+OrderedCountDbKey = PartitionId
+OrderedCountDbValue = int
+
+RefDbKey = EventId
+RefDbValue = typing.Set[EventRef]
+
+encode_system_db_key = encoder.encode_system_db_key
+encode_system_db_value = encoder.encode_system_db_value
+encode_latest_data_db_key = encoder.encode_latest_data_db_key
+encode_latest_data_db_value = encoder.encode_latest_data_db_value
+encode_latest_type_db_key = encoder.encode_latest_type_db_key
+encode_latest_type_db_value = encoder.encode_latest_type_db_value
+encode_ordered_data_db_key = encoder.encode_ordered_data_db_key
+encode_ordered_data_db_value = encoder.encode_ordered_data_db_value
+encode_ordered_partition_db_key = encoder.encode_ordered_partition_db_key
+encode_ordered_partition_db_value = encoder.encode_ordered_partition_db_value
+encode_ordered_count_db_key = encoder.encode_ordered_count_db_key
+encode_ordered_count_db_value = encoder.encode_ordered_count_db_value
+encode_ref_db_key = encoder.encode_ref_db_key
+encode_ref_db_value = encoder.encode_ref_db_value
+
+decode_system_db_value = encoder.decode_system_db_value
+decode_ref_db_value = encoder.decode_ref_db_value
+
+
+open_db = common.ext_open_db
+
+
+def create_env(path: Path):
+    max_dbs = len(common.DbType)
+    max_db_size = (512 * 1024 * 1024 * 1024
+                   if platform.architecture()[0] == '64bit'
+                   else 1024 * 1024 * 1024)
+    return lmdb.Environment(str(path),
+                            map_size=max_db_size,
+                            subdir=False,
+                            max_dbs=max_dbs)
