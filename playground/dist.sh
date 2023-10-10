9cd831e1a39fb0e546e7602a71bea12d37c48e26
bozo.kopic <bozo@kopic.xyz>
2023-10-10 11:20:19 +0200

    playground dist update

--- playground/dist.sh
+++ playground/dist.sh
@@ -4,17 +4,9 @@ set -e
 
 . $(dirname -- "$0")/env.sh
 
-TARGETS="linux_gnu_x86_64:cp38
-         linux_gnu_x86_64:cp39
-         linux_gnu_x86_64:cp310
-         linux_musl_x86_64:cp38
-         linux_musl_x86_64:cp39
+TARGETS="linux_gnu_x86_64:cp310
          linux_musl_x86_64:cp310
-         linux_gnu_aarch64:cp39
-         linux_gnu_aarch64:cp38
          linux_gnu_aarch64:cp310
-         windows_amd64:cp38
-         windows_amd64:cp39
          windows_amd64:cp310"
 
 cd $ROOT_PATH
@@ -29,9 +21,7 @@ for TARGET in $TARGETS; do
     cp $ROOT_PATH/build/py/dist/*.whl $DIST_PATH
 done
 
-IMAGES="linux/arm/v7/build-hat-event:debian11-cpy3.8
-        linux/arm/v7/build-hat-event:debian11-cpy3.9
-        linux/arm/v7/build-hat-event:debian11-cpy3.10"
+IMAGES="linux/arm/v7/build-hat-event:debian11-cpy3.10"
 
 for IMAGE in $IMAGES; do
     $PYTHON -m doit clean_all
